9be28cdc6918a02c83ce188870119dbfbb2db8ec
dsuccar <danielsuccar@icloud.com>
2020-04-23 15:10:39 -0400

    first challange done

--- src/index.js
+++ src/index.js
@@ -12,4 +12,47 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+  fetchToys();
+
 });
+
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(toyArray =>
+      toyArray.forEach(toy => {
+        renderToy(toy)
+      })
+    )
+}
+
+function renderToy(toy) {
+
+  // create card
+  let div = document.createElement("div")
+  div.className = "card"
+
+  //img
+  let img = document.createElement("img")
+  img.className = "toy-avatar"
+
+  img.src = toy.image
+  //btn
+  let btn = document.createElement("button")
+  btn.className = "like-btn"
+  btn.innerText ="Like <3"
+  //h2 tag
+  let h2 = document.createElement("h2")
+  h2.innerText = toy.name
+  //p tag
+  let p = document.createElement("p")
+  p.innerText = toy.likes
+  // append card to toy-collection div
+  let toyCollection = document.getElementById("toy-collection")
+  toyCollection.appendChild(div)
+
+  div.append(img, btn, h2, p)
+
+
+}
